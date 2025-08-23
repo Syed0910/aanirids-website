2173ce348282ca848ab58187e13ae6957710d8d2
Muskan <muskanaanirids07@gmail.com.com>
2025-08-23 15:14:26 +0530

    commit by muskan

--- src/routes/radcheck.routes.js
+++ src/routes/radcheck.routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const radcheckController = require('../controllers/radcheck.controller');
 
 router.get('/', radcheckController.getAll);
-router.get('/:id', radcheckController.getById);
-router.post('/', radcheckController.create);
-router.put('/:id', radcheckController.update);
-router.delete('/:id', radcheckController.delete);
+// router.get('/:id', radcheckController.getById);
+// router.post('/', radcheckController.create);
+// router.put('/:id', radcheckController.update);
+// router.delete('/:id', radcheckController.delete);
 
 module.exports = router;
